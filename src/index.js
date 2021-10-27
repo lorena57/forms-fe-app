350d2ad102d14223e2eaa4196b4fdf75e5b56de2
Lorena C <lorena@eunknown.org>
2021-10-26 20:41:59 -0700

    remove old code

--- src/index.js
+++ src/index.js
@@ -3,25 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import SignUp from './components/auth/SignUp';
+import Login from './components/auth/Login';
 // import { Provider } from 'react-redux';
 // import store from './store';
 import App from './App';
 import Welcome from './components/Welcome';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
       <Route path="/" exact component={Welcome} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/login" component={Login} />
     </App>
   </BrowserRouter>,
   document.getElementById('root')
